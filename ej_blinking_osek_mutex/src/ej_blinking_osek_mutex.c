c86b4e2276cda78049a3a7b57f86fd2ac990ee60
Leandro Medus <leomedus@gmail.com>
2017-10-13 10:27:18 -0300

    freeOSEK blinking example with shared resources problems

---
+++ ej_blinking_osek_mutex/src/ej_blinking_osek_mutex.c
@@ -0,0 +1,191 @@
+/* Copyright 2014, Mariano Cerdeiro
+ * Copyright 2014, Pablo Ridolfi
+ * Copyright 2014, Juan Cecconi
+ * Copyright 2014, Gustavo Muro
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** \brief Blinking_echo example source file
+ **
+ ** This is a mini example of the CIAA Firmware.
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Blinking Blinking_echo example source file
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe         Mariano Cerdeiro
+ * PR           Pablo Ridolfi
+ * JuCe         Juan Cecconi
+ * GMuro        Gustavo Muro
+ * ErPe         Eric Pernia
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20150603 v0.0.3   ErPe change uint8 type by uint8_t
+ *                        in line 172
+ * 20141019 v0.0.2   JuCe add printf in each task,
+ *                        remove trailing spaces
+ * 20140731 v0.0.1   PR   first functional version
+ */
+
+/*==================[inclusions]=============================================*/
+#include <ej_blinking_osek_mutex.h>         /* <= own header */
+#include "os.h"               /* <= operating system header */
+#include "led.h"
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+/** \brief Main function
+ *
+ * This is the main entry point of the software.
+ *
+ * \returns 0
+ *
+ * \remarks This function never returns. Return value is only to avoid compiler
+ *          warnings or errors.
+ */
+int main(void)
+{
+   /* Starts the operating system in the Application Mode 1 */
+   /* This example has only one Application Mode */
+   StartOS(Normal);
+
+   /* StartOs shall never returns, but to avoid compiler warnings or errors
+    * 0 is returned */
+   return 0;
+}
+
+/** \brief Error Hook function
+ *
+ * This fucntion is called from the os if an os interface (API) returns an
+ * error. Is for debugging proposes. If called this function triggers a
+ * ShutdownOs which ends in a while(1).
+ *
+ * The values:
+ *    OSErrorGetServiceId
+ *    OSErrorGetParam1
+ *    OSErrorGetParam2
+ *    OSErrorGetParam3
+ *    OSErrorGetRet
+ *
+ * will provide you the interface, the input parameters and the returned value.
+ * For more details see the OSEK specification:
+ * http://portal.osek-vdx.org/files/pdf/specs/os223.pdf
+ *
+ */
+void ErrorHook(void)
+{
+    Led_Toggle(RED_LED);
+   // ciaaPOSIX_printf("ErrorHook was called\n");
+   // ciaaPOSIX_printf("Service: %d, P1: %d, P2: %d, P3: %d, RET: %d\n", OSErrorGetServiceId(), OSErrorGetParam1(), OSErrorGetParam2(), OSErrorGetParam3(), OSErrorGetRet());
+   ShutdownOS(0);
+}
+
+/** \brief Initial task
+ *
+ * This task is started automatically in the application mode 1.
+ */
+TASK(Configuracion)
+{
+
+   /** Initializations */
+   Init_Leds();
+
+
+   /* activate periodic task:
+    *  - for the first time after 350 ticks (350 ms)
+    *  - and then every 250 ticks (250 ms)
+    */
+   SetRelAlarm(ActivarTareaRojoBlinking, 200, 700);
+   SetRelAlarm(ActivarTareaAzulBlinking, 100, 900);
+
+   /* terminate task */
+   TerminateTask();
+}
+
+/** \brief Periodic Task
+ *
+ * This task is started automatically every time that the alarm
+ * ActivatePeriodicTask expires.
+ *
+ */
+TASK(TareaRojoBlinking)
+{
+	uint32 contador;
+
+   Led_On(RGB_R_LED);
+   for(contador = 0; contador < 5000000; contador++);
+   Led_Off(RGB_R_LED);
+
+   /* terminate task */
+   TerminateTask();
+}
+
+TASK(TareaAzulBlinking)
+{
+	uint32 contador;
+
+   Led_On(RGB_B_LED);
+   for(contador = 0; contador < 5000000; contador++);
+   Led_Off(RGB_B_LED);
+
+   /* terminate task */
+   TerminateTask();
+}
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
